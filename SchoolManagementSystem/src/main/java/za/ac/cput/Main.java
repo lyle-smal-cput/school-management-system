610e5b3c1af16b5596fb2103edfb53d078d35248
lyle-smal-cput <223236012@mycput.ac.za>
2025-03-27 17:02:53 +0200

    Updated Project

--- SchoolManagementSystem/src/main/java/za/ac/cput/Main.java
+++ SchoolManagementSystem/src/main/java/za/ac/cput/Main.java
@@ -1,10 +1,5 @@
 package za.ac.cput;
 
-import za.ac.cput.domain.*;
-import za.ac.cput.factory.TeacherFactory;
-
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
     }
