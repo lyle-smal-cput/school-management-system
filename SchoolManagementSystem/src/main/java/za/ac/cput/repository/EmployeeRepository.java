55e14bf8a4161baae71b8183a80688d279e55c21
lyle-smal-cput <223236012@mycput.ac.za>
2025-03-28 06:55:11 +0200

    Updated Project

--- SchoolManagementSystem/src/main/java/za/ac/cput/repository/EmployeeRepository.java
+++ SchoolManagementSystem/src/main/java/za/ac/cput/repository/EmployeeRepository.java
@@ -4,7 +4,7 @@ import za.ac.cput.domain.*;
 
 import java.util.*;
 
-public class EmployeeRepository {
+public class EmployeeRepository implements IRepository{
 
     private final Map<String, Employee> employeeDatabase = new HashMap<>();
 
@@ -14,5 +14,25 @@ public class EmployeeRepository {
 
     public void findByEmpNum(String empNum) {
         employeeDatabase.get(empNum);
+    }
+
+    @Override
+    public Object create(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object read(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object o) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Object o) {
+        return false;
     }
 }
