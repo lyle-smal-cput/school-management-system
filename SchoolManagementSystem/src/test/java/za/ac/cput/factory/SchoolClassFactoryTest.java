0788ef15a47d33dd456f417816066b1d66b8e01f
rasmenis <rasmenisiya722@gmail.com>
2025-03-28 21:25:33 +0200

    Updated Project

---
+++ SchoolManagementSystem/src/test/java/za/ac/cput/factory/SchoolClassFactoryTest.java
@@ -0,0 +1,49 @@
+package za.ac.cput.factory;
+/* SchoolClassFactoryTest.java
+FactoryTest
+Author: Siya Rasmeni(222906073)
+Date: 28 March 2025
+*/
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.SchoolClass;
+import za.ac.cput.domain.Subject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchoolClassFactoryTest {
+    private static Subject subject1;
+    private static Subject subject2;
+    private static SchoolClass schoolClass1;
+    private static SchoolClass schoolClass2;
+    private static List<Subject> subjectList1;
+    private static List<Subject> subjectList2;
+
+    @BeforeAll
+    static void setUp() {
+        subject1 = SubjectFactory.createSubject("1L", "ADP2", "Applications Development 2", 8);
+        subject2 = SubjectFactory.createSubject("2L", "ICE", "Applications Development 3", 9);
+
+        subjectList1 = new ArrayList<Subject>();
+        subjectList2 = new ArrayList<Subject>();
+
+        subjectList1.add(subject1);
+        subjectList2.add(subject2);
+
+        schoolClass1 = SchoolClassFactory.build("L1", subjectList1, 9);
+        schoolClass2 = SchoolClassFactory.build("L2", subjectList2, 10);
+    }
+
+    @Test
+    void build() {
+        assertNotNull(schoolClass1);
+        assertNotNull(schoolClass2);
+        System.out.println(schoolClass1);
+        System.out.println(schoolClass2);
+    }
+
+}
