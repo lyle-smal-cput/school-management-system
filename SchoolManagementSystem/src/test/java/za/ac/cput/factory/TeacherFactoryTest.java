007a610ec4d87dfb54850b002debfa5efb307f9d
lyle-smal-cput <223236012@mycput.ac.za>
2025-03-28 18:36:26 +0200

    Updated Project

--- SchoolManagementSystem/src/test/java/za/ac/cput/factory/TeacherFactoryTest.java
+++ SchoolManagementSystem/src/test/java/za/ac/cput/factory/TeacherFactoryTest.java
@@ -1,5 +1,9 @@
 package za.ac.cput.factory;
-
+/* TeacherFactoryTest.java
+Teacher Factory  test class
+Author: Lyle Jason Smal (223236012)
+Date: 28 March 2025
+*/
 import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.Address;
 import za.ac.cput.domain.Teacher;
